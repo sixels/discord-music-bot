45717eec0495be69e335b102c28c5e6a85216280
protoshark <sixels@protonmail.com>
2023-06-29 18:45:48 +0000

    Add dependency install hack

---
+++ dep-install-hack/build.rs
@@ -0,0 +1,32 @@
+fn main() {
+    // Install external dependency (in the shuttle container only)
+    if std::env::var("HOSTNAME")
+        .unwrap_or_default()
+        .contains("shuttle")
+    {
+        if !std::process::Command::new("apt")
+            .arg("install")
+            .arg("-y")
+            .arg("libopus-dev")
+            .arg("ffmpeg")
+            .status()
+            .expect("failed to run apt")
+            .success()
+        {
+            panic!("failed to install dependencies")
+        }
+
+        if !std::process::Command::new("python3")
+            .arg("-m")
+            .arg("pip")
+            .arg("install")
+            .arg("-U")
+            .arg("yt-dlp")
+            .status()
+            .expect("failed to run python3")
+            .success()
+        {
+            panic!("failed to install yt-dlp")
+        }
+    }
+}
